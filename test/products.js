da795bd951bef3d9e33d07b5900bac3f51014de8
lmatteis <lmatteis@gmail.com>
2018-03-07 23:09:07 +0100

    Add mock products

---
+++ test/products.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'FR1': {
+    productCode: 'FR1',
+    name: 'Fruit tea',
+    price: 3.11
+  },
+  'SR1': {
+    productCode: 'SR1',
+    name: 'Strawberries',
+    price: 5.00
+  },
+  'CF1': {
+    productCode: 'CF1',
+    name: 'Coffee',
+    price: 11.23
+  },
+}
